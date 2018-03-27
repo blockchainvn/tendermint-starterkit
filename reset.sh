b6823a9aa161f7379c824f4eea9944a126b2c0c5
John Tu <tupt@qsoftvietnam.com>
2018-03-27 17:28:56 +0700

    unsafe reset all

--- reset.sh
+++ reset.sh
@@ -1,6 +1,5 @@
 nodes=(node1 node2 node3 node4)
 for node in ${nodes[@]};do
-  echo "Reseting $node"
-  rm -rf ./$node/data/*
-  tendermint unsafe_reset_priv_validator --home ./$node
+  echo "Reseting $node"  
+  tendermint unsafe_reset_all --home ./$node
 done
